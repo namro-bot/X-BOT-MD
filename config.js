9ed170c1869c5c4a522abf5824b0e58e02a2dc17
NAMRO <139112152+namro-bot@users.noreply.github.com>
2024-04-14 07:53:02 +0200

    Aktualisieren von config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   mongodb: process.env.MONGODB_URL || "",
   BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4;https://i.imgur.com/MsNQ8wf.jpg",
   STICKER_DATA: process.env.STICKER_DATA || "X-BOTMD;ASWIN SPARKY",
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "X-BOT-MD:BishopGrill",
   MENTION: process.env.MENTION || "true",
   MENTION_DATA: process.env.MENTION_DATA || 'X-BOT-MD;MULTI-DEVICE BOT BY TEAM EX-BOT-Z;https://wa.me/917012984396;https://i.imgur.com/3cMVKBk.jpeg',
   MENTION_AUDIO: global.mentionaudio,
